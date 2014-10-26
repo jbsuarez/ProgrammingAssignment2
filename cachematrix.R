a993a95eb2f07fbeef1c607f7b5ccc829c6e9932
jbsuarez <jbsuarez@users.noreply.github.com>
2014-10-26 11:53:32 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+#The following two functions are a way of using the scoping rules in R to
+#create a cacheable version of a Matrix's inverse.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+#makeCaheMatrix takes a matrix argument, calculates its inverse, and caches
+#the result.
 
+makeCacheMatrix <- function(u = matrix()){
+  minv <- NULL
+  
+  set <- function(y) {
+    u <<- y
+    minv <<- NULL
+  }
+  
+  get <- function() u
+  setinv <- function(solve) minv <<- solve
+  getinv <- function() minv
+  
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+  
+  
 }
 
+#This functions solves for the inverse of the Matrix created by makeCacheMatrix.
+#if no value is stored, the solve() function will run, otherwise it will return the value
+#of minv
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(u, ...){
+  minv <- u$getinv()
+  
+  if(!is.null(minv)) {
+    message("retrieving cached matrix")
+    return(minv)
+  }
+  
+  data <- u$get()
+  minv <- solve(data, ...)
+  u$setinv(minv)
+  minv
 }
